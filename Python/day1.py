9eaa031e40dc49271f95a81912f2865c9135cfe0
bharatanand <bharatanand@outlook.in>
2022-07-04 13:42:00 +0530

    this is file of new codes

---
+++ Python/day1.py
@@ -0,0 +1,11 @@
+print("Day 1 - Python Print Function")
+print("The function is declared like this:")
+print("print('what to print')")
+
+
+
+
+
+# Day 1 - Python Print Function
+#The function is declared like this:
+#print('what to print')
